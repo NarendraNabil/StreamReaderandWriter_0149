36a1d078136c8ea1eb5f60ca74ff1fc627b33c05
NarendraNabil <narendranabilputrawardana@gmail.com>
2025-06-10 20:20:30 +0700

    melakukan perulangan setiap baris

--- ioFile.cpp
+++ ioFile.cpp
@@ -29,3 +29,10 @@ int main () {
     cout << endl << ">= Membuka dan membaca file " << endl;
     //jika file ada maka
     if (infile.is_open())
+    {
+        // melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            //dan tampilkan disini
+            cout << baris << '\n';
+        }
