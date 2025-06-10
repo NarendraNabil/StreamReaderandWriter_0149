6e225f6447a63ead86f976f859a3ba9674088348
NarendraNabil <narendranabilputrawardana@gmail.com>
2025-06-10 20:25:14 +0700

    membuat unlimited loop untuk menulis

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -13,3 +13,6 @@ int main () {
     // menunjuk ke sebuah nama file
     outfile.open(NamaFile + ".txt", ios::out);
     cout << ">= Menulis File \'n\' untuk keluar" << endl;
+    //unlimited loop untuk menulis
+    while (true) {
+        cout << "- ";
