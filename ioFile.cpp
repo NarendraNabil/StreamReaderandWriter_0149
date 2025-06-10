2a3fc16b0b487a9ce4922af29368e0095d2b737c
NarendraNabil <narendranabilputrawardana@gmail.com>
2025-06-10 18:07:45 +0700

    menulis dan memasukkan nilai dari 'baris' ke dalam file

--- ioFile.cpp
+++ ioFile.cpp
@@ -17,3 +17,6 @@ int main () {
         getline(cin, baris);
         //loop akan berhenti jika anda memasukkan karakter q
         if (baris == "q") break;
+        // menulis dan memasukkan nilai dari 'baris' kedalam file
+        outfile << baris << endl;
+    }
